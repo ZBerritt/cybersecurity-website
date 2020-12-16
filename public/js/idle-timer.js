9560a5d60ee1577b76516f4666a0e642f438e2b9
ZBerritt <zberritt@gmail.com>
2020-12-16 10:02:30 -0500

    Added menu

---
+++ public/js/idle-timer.js
@@ -0,0 +1,15 @@
+var idleTime = 0;
+(function(){
+    document.addEventListener("mousedown", e => {
+        idleTime = 0;
+    });
+
+    document.addEventListener("mousemove", e => {
+        idleTime = 0;
+    });
+
+    let idleTimer = setInterval(function() {
+        idleTime++;
+        if (idleTime >= 6) window.location.href = "./" // # of 30 second intervals before reload
+    }, 30000)
+}())
