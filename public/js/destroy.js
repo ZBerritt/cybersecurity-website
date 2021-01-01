6a9fa932f3ecf8a458dd39e300b86436b53bf115
ZBerritt <zberritt@gmail.com>
2021-01-01 14:15:38 -0500

    Mostly completed first destroy prototype

--- public/js/destroy.js
+++ public/js/destroy.js
@@ -1,6 +1,10 @@
 var stageImage = document.getElementById("stage-image");
 var boxOne = document.getElementById("box-1");
 var boxTwo = document.getElementById("box-2");
+var boxClosed = [document.getElementById("box-1-closed"), document.getElementById("box-2-closed")];
+var boxOpen = [document.getElementById("box-1-open"), document.getElementById("box-2-open")]
+var code1 = document.getElementById("input-1");
+var code2 = document.getElementById("input-2");
 var hacking = false; // Is the user hacking
 var codes = ["secret", "code"] // Dont make this case sensitive you dumb idiot
 var complete = [false, false];
@@ -28,5 +32,31 @@ function closeBox() {
 }
 
 function inputCode(box) {
-    
+    var input;
+    if (box === 1) {
+        input = code1.value;
+    } else {
+        input = code2.value;
+    }
+    if (input.toLowerCase() === codes[box - 1]) {
+        // Correct code
+        boxClosed[box - 1].style.display = "none";
+        boxOpen[box - 1].style.display = "block";
+    }
+}
+
+function switchOne() {
+    complete[0] = true;
+    document.getElementById("switch-1").src = "../public/assets/destroy/Switch ON.png"
+    if (complete[0] && complete[1]) {
+        // Result
+    }
+}
+
+function switchTwo() {
+    complete[1] = true;
+    document.getElementById("switch-2").src = "../public/assets/destroy/Switch ON.png"
+    if (complete[0] && complete[1]) {
+        // Result
+    }
 }
